a22b926f425d9ec28389cb788dfc051a8bf8a980
Lucy Bridges <lucy.bridges@thedatalab.org>
2022-04-12 10:19:41 +0100

    Rename project to interactive

--- interactive/asgi.py
+++ interactive/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for opensafely_interactive project.
+ASGI config for interactive project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -12,6 +12,6 @@ import os
 from django.core.asgi import get_asgi_application
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "opensafely_interactive.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "interactive.settings")
 
 application = get_asgi_application()
