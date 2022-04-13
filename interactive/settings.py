e16323078d4ce5d306732d190983e8bfa2bd9fa2
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2022-04-13 12:11:28 +0100

    Set up Whitenoise with favicons

--- interactive/settings.py
+++ interactive/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from environs import Env, EnvError
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -126,8 +127,37 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/"
 
-STATIC_URL = "static/"
+# Insert Whitenoise Middleware.
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+WHITENOISE_SKIP_COMPRESS_EXTENSIONS = (
+    "br",
+    "bz2",
+    "css.map",
+    "flv",
+    "gif",
+    "gz",
+    "ico",
+    "jpeg",
+    "jpg",
+    "js.map",
+    "json",
+    "map",
+    "png",
+    "svg",
+    "swf",
+    "tbz",
+    "tgz",
+    "webmanifest",
+    "webp",
+    "woff",
+    "woff2",
+    "xz",
+    "zip",
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
