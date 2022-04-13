a65ce888f15c794b6123bd0bc4897bcee10dc8e4
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2022-04-13 14:49:39 +0100

    Set up local fonts

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   plugins: {
+    "postcss-url": [
+      {
+        // Inline font files when working locally
+        filter: (t) => {
+          return /([a-zA-Z0-9\s_\\.\-\(\):])+(.woff|.woff2)$/i.test(t.url);
+        },
+        url: process.env.NODE_ENV !== "production" ? "inline" : null,
+      },
+    ],
     tailwindcss: {},
     autoprefixer: {},
   },
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./interactive/templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Public Sans", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        oxford: {
+          DEFAULT: "#002147",
+          50: "#f1f7ff",
+          100: "#cfe5ff",
+          200: "#9ccaff",
+          300: "#69afff",
+          400: "#3693ff",
+          500: "#0378ff",
+          600: "#0058be",
+          700: "#00397a",
+          800: "#002147",
+          900: "#001936",
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
