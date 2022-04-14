54a4abdd481e9126eaf25212bc266effbdb59469
Lucy Bridges <lucy.bridges@thedatalab.org>
2022-04-14 12:15:23 +0100

    Use gunicorn to run the app in production
    This also sets up formatted log messages for production.

--- docker/entrypoint.prod.sh
+++ docker/entrypoint.prod.sh
@@ -2,8 +2,7 @@
 
 set -euo pipefail
 
-./manage.py check --deploy
 ./manage.py migrate
 ./manage.py collectstatic --no-input
 
-exec "$@"
+exec gunicorn interactive.wsgi --config=deploy/gunicorn.conf.py
