b57b94798bd2587a6565755e5e8829e3fd4aa3a6
Peter Inglesby <peter.inglesby@gmail.com>
2022-04-19 16:45:50 +0100

    Reorder INSTALLED_APPS
    From the [Django docs](https://docs.djangoproject.com/en/4.0/ref/settings/):
    
    > When several applications provide different versions of the same
    > resource (template, static file, management command, translation), the
    > application listed first in `INSTALLED_APPS` has precedence.
    
    This order allows other packages to override Django's defaults, and
    allows us to override other packages and Django's defaults.

--- interactive/settings.py
+++ interactive/settings.py
@@ -45,16 +45,16 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "interactive",
+    "whitenoise.runserver_nostatic",
+    "django.contrib.staticfiles",
+    "django_extensions",
+    "django_vite",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "whitenoise.runserver_nostatic",
-    "django.contrib.staticfiles",
-    "django_extensions",
-    "django_vite",
-    "interactive",
 ]
 
 MIDDLEWARE = [
