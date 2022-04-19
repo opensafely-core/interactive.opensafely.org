a8415e702c617697ecb48a8926486ebfe03b2d21
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2022-04-19 12:17:52 +0100

    Add example toast notification

--- interactive/settings.py
+++ interactive/settings.py
@@ -200,3 +200,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Authentication
 LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
--- interactive/urls.py
+++ interactive/urls.py
@@ -26,11 +26,15 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("admin/", admin.site.urls),
     path(
-        "accounts/login/",
+        "login/",
         auth_views.LoginView.as_view(redirect_authenticated_user=True),
         name="login",
     ),
-    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(),
+        name="logout",
+    ),
     # path(
     #     "accounts/password-reset/",
     #     auth_views.PasswordResetView.as_view(),
