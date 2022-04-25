c2b7a4f814db21e27c1dc7388af19ae2e533b45a
Lucy Bridges <lucy.bridges@thedatalab.org>
2022-04-25 13:22:45 +0100

    Reformat form and add info text

--- interactive/models.py
+++ interactive/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class AnalysisRequest(models.Model):
 
-    title = models.CharField(max_length=100)
-    codelist = models.CharField(max_length=255)
+    title = models.CharField(max_length=100, verbose_name="Analysis title")
+    codelist = models.CharField(max_length=255, verbose_name="Codelist")
 
     def __str__(self) -> str:
         return f"{self.title} ({self.codelist})"
