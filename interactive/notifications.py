a22757bc2ac03876d99a8c7e7f54a1f21be3288b
Lucy Bridges <lucy.bridges@thedatalab.org>
2022-04-28 16:17:09 +0100

    Extend slack link creator for email addresses

--- interactive/notifications.py
+++ interactive/notifications.py
@@ -19,6 +19,6 @@ def notify_analysis_request_submitted(title, codelist, created_by):
 
 
 def notify_register_interest_submitted(full_name, job_title, organisation, email):
-    full_name_link = slack.link(email, full_name)
+    full_name_link = slack.link(email, full_name, is_email=True)
     message = f"{full_name_link} ({job_title}) from {organisation} has registered their interest in using OpenSAFELY Interactive"
     slack.post(text=message)
