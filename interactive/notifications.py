af4172b9f3ae55df3bd9cf33e49ae3858744d599
Lucy Bridges <lucy.bridges@thedatalab.org>
2022-05-05 11:59:03 +0100

    Generate text email from html template

--- interactive/notifications.py
+++ interactive/notifications.py
@@ -1,3 +1,4 @@
+import html2text
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from environs import Env
@@ -28,8 +29,8 @@ def notify_registration_request_submitted(full_name, job_title, organisation, em
 
 def send_welcome_email(email, context):
     subject = "Welcome to OpenSAFELY Interactive"
-    text_body = render_to_string("emails/welcome_email.txt", context)
     html_body = render_to_string("emails/welcome_email.html", context)
+    text_body = _convert_html(html_body)
 
     msg = EmailMultiAlternatives(
         subject=subject,
@@ -39,3 +40,13 @@ def send_welcome_email(email, context):
     )
     msg.attach_alternative(html_body, "text/html")
     msg.send()
+
+
+def _convert_html(raw_html):
+    text_maker = html2text.HTML2Text()
+    text_maker.ignore_images = True
+    text_maker.ignore_emphasis = True
+    text_maker.ignore_links = False
+    text_maker.body_width = 0
+    raw_text = text_maker.handle(raw_html)
+    return raw_text
