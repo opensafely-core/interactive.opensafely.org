cfd10e537baaad1aee674b8726a198148fadcabe
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2022-06-20 09:34:35 +0100

    Prevent users from double-clicking form submit

--- assets/src/scripts/main.js
+++ assets/src/scripts/main.js
@@ -17,3 +17,24 @@ if (document.location.hostname === "interactive.opensafely.org") {
 
   document.head.appendChild(script);
 }
+
+document.querySelectorAll("input[type='submit']").forEach((submit) => {
+  const formEl = submit.parentElement.closest("form");
+  let submitClick = false;
+
+  submit.addEventListener("click", (e) => {
+
+    // Check if required fields have been filled
+    if (formEl.checkValidity()) {
+
+      // If click has already been submitted
+      // then disable the button clicks
+      if (submitClick) {
+        e.preventDefault();
+        submit.disabled = true;
+      }
+
+      submitClick = true;
+    }
+  });
+});
