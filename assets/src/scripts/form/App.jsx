ef8a80e2a8f184e62f5a2b8135cf5d1f0741c5e6
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2022-12-23 10:19:22 +0000

    Remove React Form code

--- assets/src/scripts/form/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Select } from "./components/Select";
-
-function App({ choices, data, errors }) {
-  return <Select choices={choices} data={data} errors={errors} />;
-}
-
-export default App;
